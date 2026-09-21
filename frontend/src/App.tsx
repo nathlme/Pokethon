bf4008ea2199c9e42e92d811c7b1685f12253b27
lanathan <nathan.lamarche@ynov.com>
2026-09-21 11:47:16 +0200

    test valide

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import Pokedex from "./pages/Pokedex";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PokemonDetail from "./pages/PokemonDetail";
 import PrivateRoute from "./components/PrivateRoute";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
     const [darkMode, setDarkMode] = useState(
@@ -20,17 +22,20 @@ function App() {
         <>
             <Navbar darkMode={darkMode} onToggleTheme={() => setDarkMode(!darkMode)} />
                 <Pokedex />
-            <main className="page">...</main>
-            <BrowserRouter>
-            <Routes>
-                <Route element={<PrivateRoute />}>
-                    <Route path="/pokedex" element={<div>Pokédex (à venir)</div>} />
-                    <Route path="/pokemon/:id" element={<PokemonDetail />} />
-                    <Route path="/collection" element={<div>Collection (à venir)</div>} />
-                    <Route path="/trades" element={<div>Échanges (à venir)</div>} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+            <main className="page"> 
+                <Routes>
+
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+
+                    <Route element={<PrivateRoute />}>
+                        <Route path="/pokedex" element={<div>Pokédex (à venir)</div>} />
+                        <Route path="/pokemon/:id" element={<PokemonDetail />} />
+                        <Route path="/collection" element={<div>Collection (à venir)</div>} />
+                        <Route path="/trades" element={<div>Échanges (à venir)</div>} />
+                    </Route>
+                </Routes>
+            </main>
         </> 
     );
 }
