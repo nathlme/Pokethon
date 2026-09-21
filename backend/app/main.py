8343e6279045fab20d5080366fd92d636b147284
DECUNG Tessa <tessa.decung@ynov.com>
2026-09-21 09:52:11 +0200

    feat/team-teamslot: updated main.py, registered team and team-slot routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app import models
+from app.routers import team, team_slot
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+app.include_router(team.router)
+app.include_router(team_slot.router)
+
 @app.get("/")
 def root():
-    return {"message": "Pokethon API is running"}
-
-# Crée les tables au démarrage
+    return {"message": "PokeThon API is running"}
