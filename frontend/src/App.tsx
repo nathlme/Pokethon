1ad33f7ac9ea0d1e52ca26e1ec437b7a81780ae6
felixpouteau <felix.pouteau@epitech.eu>
2026-09-21 11:21:38 +0200

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,24 @@
-// App.tsx
-import { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PokemonDetail from "./pages/PokemonDetail";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
     return (
-        <>
-            <Navbar darkMode={darkMode} onToggleTheme={() => setDarkMode(!darkMode)} />
-            <main className="page">...</main>
-        </>        
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+
+                <Route element={<PrivateRoute />}>
+                    <Route path="/pokedex" element={<div>Pokédex (à venir)</div>} />
+                    <Route path="/pokemon/:id" element={<PokemonDetail />} />
+                    <Route path="/collection" element={<div>Collection (à venir)</div>} />
+                    <Route path="/trades" element={<div>Échanges (à venir)</div>} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
